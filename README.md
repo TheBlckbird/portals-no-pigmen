525ffe7a80ec7d319f5fb292a138960217e8bc67
TheBlckbird <theblckbrd@proton.me>
2026-06-09 19:02:34 +0200

    Initial commit (adds mixin)

---
+++ README.md
@@ -0,0 +1,7 @@
+# Piglin Free Portals
+
+Disables piglins spawning from Nether Portals in Minecraft.
+
+## LICENSE
+
+[GPL-3.0](/LICENSE)
---
+++ src/main/java/dev/theblckbird/piglinfreeportals/PiglinFreePortals.java
@@ -0,0 +1,8 @@
+package dev.theblckbird.piglinfreeportals;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(PiglinFreePortals.MODID)
+public class PiglinFreePortals {
+    public static final String MODID = "piglinfreeportals";
+}
